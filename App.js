40eb52d90c300c6cd2028f14d9417509971b3697
Aldo Vallera <75909548+kiriadix@users.noreply.github.com>
2022-11-09 01:44:14 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [textItem, settextItem] = useState('')
+  const [itemList, setItemList] = useState([])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <TextInput placeholder='Add item' style={styles.inputContainer} />
+        <Button title='Add' color='#49DA9D' />
+      </View>
+      <View style={styles.listContainer}>
+        <Text>CHUPAME EL PITO</Text>
+        <Text>CHUPAME EL PITO</Text>
+        <Text>CHUPAME EL PITO</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    padding:10, 
+    marginTop:50
   },
+  itemContainer:{
+    flexDirection:'row', 
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  inputContainer:{
+    borderBottomColor:'black',
+    borderBottomWidth:1,
+    width:280,
+    margin:8,
+  },
+  listContainer:{
+    borderColor: 'black',
+    borderRadius:10,
+    borderWidth: 1,
+    margin: 10,
+    padding:5,
+    justifyContent:'center',
+    alignItems:'center'    
+  }
+
 });
